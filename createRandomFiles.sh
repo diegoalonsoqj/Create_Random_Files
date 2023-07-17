eb17d5190a75a12f4d237e162964eff0cb90a21e
Diego Quispe <72992112+diegoalonsoqj@users.noreply.github.com>
2023-07-16 21:10:23 -0500

    v1.0
    El script createRandomFiles.sh genera una cantidad definida de archivos aleatorios con un formato y tamaño específico en una ubicación determinada por el usuario.

---
+++ createRandomFiles.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Pregunta al usuario cuántos archivos quiere crear
+echo "¿Cuántos archivos te gustaría crear?"
+read file_count
+
+# Pregunta al usuario cuál sería el tamaño máximo de los archivos en kilobytes
+echo "¿Cuál es el tamaño máximo de los archivos en kilobytes?"
+read max_size
+
+# Pregunta al usuario la ruta donde se crearán los archivos
+echo "¿Dónde te gustaría crear los archivos?"
+read dir_path
+
+# Lista de posibles extensiones de archivo
+ext_list=("txt" "doc" "pdf" "jpg" "png")
+
+# Itera la cantidad de veces solicitada por el usuario
+for ((i=1; i<=file_count; i++))
+do
+    # Elige una extensión de archivo aleatoria de la lista
+    ext=${ext_list[$RANDOM % ${#ext_list[@]}]}
+
+    # Crea un archivo con la extensión seleccionada
+    touch "$dir_path/file$i.$ext"
+
+    # Genera un tamaño aleatorio de archivo en kilobytes no mayor al tamaño máximo proporcionado por el usuario
+    size=$(shuf -i 1-$max_size -n 1)
+
+    # Aumenta el tamaño del archivo a la cantidad especificada
+    dd if=/dev/zero of="$dir_path/file$i.$ext" bs=1024 count=$size >/dev/null 2>&1
+done
+
+echo "Archivos creados con éxito en $dir_path."
